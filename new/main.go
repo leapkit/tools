583ef65b323c8f13f202685495656262d52bd7e0
Antonio Pagano <apagano@wawand.co>
2025-02-22 21:07:54 -0500

    task: adding some commments

--- new/main.go
+++ new/main.go
@@ -9,6 +9,9 @@ import (
 	"os/exec"
 )
 
+// new tool creates a new leapkit module using the gonew tool
+// and the go.leapkit.dev/template template. It receives the name of
+// the newly created module as an argument.
 func main() {
 	args := os.Args
 	if len(args) < 2 {
@@ -16,6 +19,8 @@ func main() {
 		return
 	}
 
+	// new tool invokes gonew under the hood to create a
+	// new module using the go.leapkit.dev/template template.
 	cmd := exec.Command(
 		"go", "run", "rsc.io/tmp/gonew@latest",
 		"go.leapkit.dev/template@latest",
